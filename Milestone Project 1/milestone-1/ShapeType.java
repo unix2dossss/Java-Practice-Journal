321df6b8baa36ad82e1c591434455e1b5bd80e09
unix2dossss <vandan.ubuntu@gmail.com>
2024-03-19 08:36:08 +1300

    ShapeType enum: Q3

--- Milestone Project 1/milestone-1/ShapeType.java
+++ Milestone Project 1/milestone-1/ShapeType.java
@@ -5,3 +5,16 @@
  *  ============================================================================================
  */
 //Complete the ShapeType enum
+
+enum ShapeType{
+    RECTANGLE(4), SQUARE(4), HEXAGON(6), HEPTAGON(7), OCTAGON(8);
+    private int numberOfSides;
+
+    private ShapeType(int numberOfSides){
+        this.numberOfSides = numberOfSides;
+    }
+
+    public int getNumberOfSides(){
+        return numberOfSides;
+    }
+}
