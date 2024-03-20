5d9e34a647e3cee574e697c5ae67fd3bad22bccd
unix2dossss <vandan.ubuntu@gmail.com>
2024-03-21 10:53:45 +1300

    Has-A: Q3-4.

--- L04/polyLineRun.java
+++ L04/polyLineRun.java
@@ -3,18 +3,14 @@ import java.util.ArrayList;;
 
 public class polyLineRun {
     public static void main(String[] args){
-    PolyLine line1 = new PolyLine(); 
-    line1.appendPoint(10, 20); 
-    PolyLine line2 = new PolyLine(); 
-    line2.appendPoint(10, 20);
-    System.out.println(line1);
-    System.out.println(line2);
-    System.out.println(line1 == line2);
-    line1.appendPoint(20, 30);
-    System.out.println(line1);
-    PolyLine line3 = new PolyLine();
-    System.out.println(line3);
-    System.out.println("DONE");
+        PolyLine line = new PolyLine();
+        line.appendPoint(1, 2);
+        line.appendPoint(3, 4);
+        line.appendPoint(5, 6);
+        System.out.printf("%.2f%n", line.getLength());
+        PolyLine line2 = new PolyLine();
+        line2.appendPoint(5, 6);
+        System.out.printf("%.2f%n", line2.getLength());
     }
 }
 
@@ -37,5 +33,20 @@ class PolyLine {
         }
         out.append(String.format("%s", points.get(points.size()-1).toString()));
         return out.toString();
+    }
+
+    public int getSize(){
+        return points.size();
+    }
+
+    public double getLength(){
+        double length = 0.00;
+        if (this.getSize()<=1){
+            return length;
+        }
+        for (int p = 0; p<=this.getSize()-2; p++) {
+            length += points.get(p).distance(points.get(p+1));
+        }
+        return length;
     }
 }
