85c370a54c6c305130ab38ed01670f72ccecd441
unix2dossss <vandan.ubuntu@gmail.com>
2024-03-27 19:15:38 +1300

    Subclasses: Q7.

--- Milestone Project 1/milestone-1/SquareShape.java
+++ Milestone Project 1/milestone-1/SquareShape.java
@@ -5,3 +5,12 @@
  * =============================================================================== */
 import java.awt.*;
 //Complete the SquareShape class
+
+class SquareShape extends RectangleShape{
+    public SquareShape(){
+        this(DEFAULT_X, DEFAULT_Y, DEFAULT_HEIGHT, DEFAULT_PANEL_WIDTH, DEFAULT_PANEL_HEIGHT, DEFAULT_COLOR, DEFAULT_BORDER_COLOR, DEFAULT_PATHTYPE);
+    }
+    public SquareShape(int x, int y, int s, int panelWidth, int panelHeight, Color c, Color bc, PathType pt){
+        super(x, y, s, s, panelWidth, panelHeight, c, bc, pt);
+    }
+}
